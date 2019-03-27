5b6cfcf0595a7773c8296e172df8b1da176bd80b
Chia Han <tomyjill@hotmail.com>
2019-03-27 23:02:13 +0000

    Added renderHomePage and renderPostPage functions

--- blogPost.js
+++ blogPost.js
@@ -3,48 +3,67 @@
 // https://jsonplaceholder.typicode.com/comments?postId={{ postId }}
 
 $(document).ready(function(){
+    
+    navigate('home');
+
+    $(document).on('click', '.goToPost', function() {
+        const postId = $(this).parent().attr('postId');
+        navigate('post', { postId: postId });
+
+        // $.getJSON(idUrl, function(posts) {
+        //     posts.forEach(post => {
+        //         window.open('postNewPage.html');         
+        //         $('#postIdContainer').append(
+        //             `<div class="body">${post.body}</div>
+        //             <div class="name"> Posted by ${post.name}</div>
+        //             <div class="email">${post.email}</div>`
+        //         );
+        //     });
+        // });
+    });
+
+    $(document).on('click', '#goHome', function() {
+        navigate('home');
+    });
+});
+
+const navigate = (page, params = {}) => {
+    $('#container').children().remove();
+    switch (page) {
+        case 'home':
+            renderHomePage(params);
+        break;
+        case 'post':
+            renderPostPage(params);
+        break;
+    }
+}
+        
+const renderPostPage = (params) => {
+    const { postId } = params;
+    console.log(postId);
+    // const url = 'https://jsonplaceholder.typicode.com/comments?postId=';
+    // const idUrl = url.concat(postId);
+    $('#container').append('<div>welcome to post page</div>')
+    $('#container').append('<div id="goHome">go home</div>')
+}
+
+const renderHomePage = (params) => {
     const url = 'https://jsonplaceholder.typicode.com/posts';
-
+    
 	$.getJSON(url, function(posts) {
-
         posts.forEach(post => {
-            $('#postsContainer').append(
-                `<div class='postContainer'>
+            $('#container').append(
+          //  $('#postsContainer').append(
+                `<div class='postContainer' postId=${post.id}>
                     <div class="postTitle">${post.title}</div>
                     <div class= "postBody">${post.body}</div>
                     <br>
                     <div class= "postId"> Posted by ${post.userId}</div>
-                    <button class="comment btn btn-outline-info pull-right">Go to post</button>
+                    <button class="goToPost btn btn-outline-info pull-right">Go to post</button>
                 </div>`
             );
-            console.log(post)
         });
     });
-    
-    $.getJSON(postIdUrl, function(posts) {
-        const postIdUrl = 'https://jsonplaceholder.typicode.com/comments?postId';
-        const userIdUrl = 'https://jsonplaceholder.typicode.com/users';
-        console.log(postIdUrl);
-        $('.comment').on('click', function(){
-            const comments = posts.map(post => {
-                if (post.id == postId) {
-                    posts.forEach(post => {
-                        $('#commentsContainer').append(
-                            `<div class='commentContainer'>
-                                <div class="body">${post.body}</div>
-                                <div class="name"> Posted by ${post.name}</div>
-                                <div class="email">${post.email}</div>
-                            </div>`
-                        );
-                    });
-                } 
-            })
-        });
-    });
-    
-});
-
-   
-        
-
+}
 			
