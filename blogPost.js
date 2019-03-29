4e0a56603584dc3f3a24178f79f5f1ab97f4e898
Chia Han <tomyjill@hotmail.com>
2019-03-29 22:54:39 +0000

    Chaged the btn-outline-info to btn-outline-danger for the .goHome button.

--- blogPost.js
+++ blogPost.js
@@ -43,7 +43,7 @@ const renderPostPage = (params) => {
                         <br>
                         <div class="name"> Posted by ${post.name}</div>
                         <div class="email">${post.email}</div>
-                        <button class="goHome btn btn-outline-info pull-right">Go Home</button>
+                        <button class="goHome btn btn-outline-danger pull-right">Go Home</button>
                     </div>`
                 );
             });
